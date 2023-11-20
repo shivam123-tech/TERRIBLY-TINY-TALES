5456532e906c95db9babc96701d2155009baa0b8
Shivam Bhardwaj <72145406+shivam123-tech@users.noreply.github.com>
2023-11-20 20:53:40 +0530

    Add files via upload

---
+++ project/frontend/src/components/Body.js
@@ -0,0 +1,22 @@
+import './Body.css';
+function Body(){
+    return(
+        <>
+        <div>
+        <img className="img"src ="https://images.squarespace-cdn.com/content/v1/5f54b2c198bf2728f8f3ea29/1599401725878-SR4ZCZUJZ7AMFTWRAZBU/317190_0013_3.jpg?format=1500w" />
+        <img  className="ppimg"src="https://www.realmenrealstyle.com/wp-content/uploads/2023/07/Glasses-for-Oval-shape.jpg"/>
+        <span className="username">Anuj Gosalia</span>
+        <img className='diaimg'src="https://cdn-icons-png.flaticon.com/128/5257/5257862.png"/>
+        <img className='verimg'src="https://cdn-icons-png.flaticon.com/128/5610/5610944.png"/>
+        <span className="follower">6482</span>
+        <span className="followertext">Followers</span>
+        <span className="following">245</span>
+        <span className="followingtext">Following</span>
+        <div className="text">Co-founder & CEO at Terribly Tiny Tales</div>
+        <div className="instalink">http://www.instagram.com/anujgosalia</div>
+        </div>
+        </>
+    );
+}
+
+export default Body;
---
+++ project/frontend/src/components/Navbar.js
@@ -0,0 +1,25 @@
+import './Navbar.css';
+
+function Navbar(){
+    return(
+     <>
+     <div className='nav'>
+       Hello
+       <div className='ppic'>ttt</div>
+       <div class="vl"></div>
+       <h3 className="str">S</h3>
+       <h3 className="str">T</h3>
+       <h3 className="str">O</h3>
+       <h3 className="str">R</h3>
+       <h3 className="str">I</h3>
+       <h3 className="str">E</h3>
+       <h3 className="str">S</h3>
+       <div className="Cor">Courses</div>
+     </div>
+     
+     </>
+
+    );
+}
+
+export default Navbar;
